3b258a85abd754111742a6f0e36e93a6f979826b
Seven <1162584980@qq.com>
2025-04-16 14:42:46 +0800

    init

--- initt/cmdline.py
+++ initt/cmdline.py
@@ -6,7 +6,7 @@ from jinja2 import Environment, FileSystemLoader, TemplateNotFound
 
 import questionary
 
-__VERSION__ = "0.0.1"
+__VERSION__ = "0.0.2"
 
 # Template configuration structured definition
 TEMPLATES: Dict[str, Dict[str, Union[List[str], List[Dict[str, Any]]]]] = {
